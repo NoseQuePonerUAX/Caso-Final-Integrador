9e0c7df0dee9e433863f525871ac31c30ef605ce
NoseQuePonerUAX <joserafe@myuax.com>
2024-02-21 00:26:34 +0100

    cambio1.2

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
 public class main {
     public static void main(String[] args) {
-        int diasenano = 365;
-        int horasenunano = 24;
-        int minutosenunano = 60;
-        int segundosenunano = 60;
+        int diasenanoentierra = 365;
+        int horasenanoentierra = 24;
+        int minutosenanoentierra = 60;
+        int segundosenanoentierra = 60;
     }
 }
